f400347f89a204e917278bf96717861a14feb0be
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2026-01-02 23:48:33 +0530

    Time: 4 ms (87.99%), Space: 59.6 MB (30.2%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let set=new Set()
+    let max=0;
+    let l=0;
+    for(let r=0;r<s.length;r++){
+        while(set.has(s[r])){
+            set.delete(s[l]);
+            l++;
+        }
+        set.add(s[r]);
+        max=Math.max(r-l+1,max);
+    }
+    return max;
+};
