7100dc8a735f5b3bc23a4faf5bbd406b845eed1d
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2026-01-11 18:11:07 +0530

    Time: 8 ms (76.14%), Space: 66.5 MB (12.5%) - LeetHub

---
+++ 2363-merge-similar-items/2363-merge-similar-items.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} items1
+ * @param {number[][]} items2
+ * @return {number[][]}
+ */
+var mergeSimilarItems = function(items1, items2) {
+    let map=new Map()
+
+    for(let [v,w] of items1){
+        map.set(v,(map.get(v)||0)+w)
+    }
+
+    for(let [v,w] of items2){
+        map.set(v,(map.get(v)||0)+w)
+    }
+
+    return Array.from(map.entries()).sort((a,b)=>a[0]-b[0])
+};
