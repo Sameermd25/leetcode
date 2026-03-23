6d856c609b64adbe9d978836dd31a17fe0277ded
Md Sameer <149755097+Sameermd25@users.noreply.github.com>
2026-03-23 23:05:05 +0530

    Time: 10 ms (6.19%), Space: 57.7 MB (12.19%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    s=s.trim();
+    let a=s.split(" ");
+    for(let i=0;i<a.length;){
+        if(a[i]=="") a.splice(i,1)
+        else i++;
+    }
+    let l=0;
+    let r=a.length-1;
+    while(l<r){
+        if(a[l]==" ") l++;
+        if(a[r]==" ") r--;
+        [a[l],a[r]]=[a[r],a[l]];
+        l++;
+        r--;
+    }
+    return a.join(" ")
+};
