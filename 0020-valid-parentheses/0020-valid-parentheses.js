42459ed20e5b2748d5390e8b693f10c33ab71c50
Md Sameer <149755097+Sameermd25@users.noreply.github.com>
2026-04-02 11:34:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let arr=[]
+    let map={
+        ")":"(",
+        "]":"[",
+        "}":"{"
+    }
+
+    for(let i=0;i<s.length;i++){
+        if(s[i]=="(" || s[i]=="{" ||s[i]=="["){
+            arr.push(s[i])
+        }
+        else{
+            let last=arr.pop()
+            if(map[s[i]]!=last) return false
+        }
+    }
+    return true;
+};
