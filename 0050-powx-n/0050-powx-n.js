6c904d40f7ec363e7403a4ee057f1e9e211879c0
Md Sameer <149755097+Sameermd25@users.noreply.github.com>
2026-07-23 13:11:33 +0530

    Time: 0 ms (100%), Space: 53.9 MB (47.2%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    // let res=1;
+    // if(n>0){
+    //     while(n!==0){
+    //        res*=x;
+    //        n--;
+    //     }
+    // }
+    // else{
+    //    return x**n;
+    // }
+    
+    // return res;
+    return Math.pow(x,n)
+};
