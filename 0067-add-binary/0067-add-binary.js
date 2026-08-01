0afe5cf6b071f2083f22b3dbc5410a11f26f782d
Md Sameer <149755097+Sameermd25@users.noreply.github.com>
2026-08-01 15:00:35 +0530

    Time: 1 ms (67.48%), Space: 55.3 MB (50.61%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    let res=[];
+    let i=a.length-1;
+    let j=b.length-1;
+    let carry=0
+    while(i>=0 || j>=0 || carry>0){
+        let sum=carry;
+        if(i>=0){
+            sum+=Number(a[i]);
+            i--
+        }
+        if(j>=0){
+            sum+=Number(b[j]);
+            j--
+        }
+        res.push(sum%2);
+        carry=Math.floor(sum/2)
+    }
+    return res.reverse().join("")
+};
