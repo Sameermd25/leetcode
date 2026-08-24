8642f456b0e01ae5822e502743486ef01c69b036
Md Sameer <149755097+Sameermd25@users.noreply.github.com>
2026-08-24 16:54:06 +0530

    Time: 0 ms (100%), Space: 55.3 MB (80.77%) - LeetHub

---
+++ 3798-largest-even-number/3798-largest-even-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var largestEven = function(s) {
+    for(let i=s.length-1;i>=0;i--){
+        if(Number(s[i])%2===0){
+            return s.slice(0,i+1);
+        }
+    }
+    return ""
+};
