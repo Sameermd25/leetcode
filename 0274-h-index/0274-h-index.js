672d6c59cab79ea3f3923f7198e4ab1e2207657a
Md Sameer <149755097+Sameermd25@users.noreply.github.com>
2026-08-26 19:35:40 +0530

    Time: 25 ms (6.68%), Space: 55.9 MB (7.93%) - LeetHub

---
+++ 0274-h-index/0274-h-index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations,H=0) {
+    if(H>citations.length) return citations.length
+    let count=0;
+    for(let i=0;i<citations.length;i++){
+        if(citations[i]>=H) count++;
+    }
+    console.log(count)
+    if(count>=H){
+        return hIndex(citations,H+1)
+    }else{
+        return H-1;
+    }
+
+};
