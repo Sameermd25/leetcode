0ff1e72fc318a55ac668b7c6e2de63838f11ea9f
Md Sameer <149755097+Sameermd25@users.noreply.github.com>
2026-09-13 00:47:47 +0530

    Time: 2 ms (87.65%), Space: 58.8 MB (70.28%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let res=Infinity;
+    let l=0;
+    let sum=0
+    for(let r=0;r<nums.length;r++){
+        sum+=nums[r]
+        while(sum>=target){
+            res=Math.min(res,r-l+1)
+            sum-=nums[l];
+            l++
+        }
+    }
+    return res==Infinity?0:res
+};
