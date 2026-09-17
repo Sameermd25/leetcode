bd71a5b721ecdc57678743237bbb468234a587e7
Md Sameer <149755097+Sameermd25@users.noreply.github.com>
2026-09-17 21:40:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -3,19 +3,15 @@
  * @return {number}
  */
 var jump = function(nums) {
-    let jumps = 0;
-    let currentEnd = 0;
-    let farthest = 0;
-
-    for (let i = 0; i < nums.length - 1; i++) {
-        farthest = Math.max(farthest, i + nums[i]);
-
-        //since we reached currEnd we need to jump and change farthest
-        if (i === currentEnd) {
-            jumps++;
-            currentEnd = farthest;
+    let j=1;
+    let maxR=nums[0]
+    for(let i=1;i<nums.length;i++){
+        let curr=nums[i]+i
+        maxR=Math.max(maxR,curr)
+        if(maxR>=nums.length-1){
+            j++;
+            return j;
         }
     }
-
-    return jumps;
+    return j;
 };
