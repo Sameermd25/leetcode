170ec90c4d29506e41e07b395a1c484245017e70
Md Sameer <149755097+Sameermd25@users.noreply.github.com>
2026-09-20 16:49:16 +0530

    Time: 38 ms (44.28%), Space: 70.9 MB (93.65%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,30 +3,33 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums.sort((a,b)=>a-b);
-    let res=[];
-    for(let i=0;i<nums.length-2;i++){
-        if(nums[i]==nums[i-1]){
+    nums.sort((a,b)=>a-b)
+    //console.log(nums)
+    let res = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (i>0 && nums[i]===nums[i - 1]) {
             continue;
         }
         let l=i+1;
         let r=nums.length-1;
+
         while(l<r){
-            let sum=nums[i]+nums[l]+nums[r];
+            let sum=nums[i]+nums[l]+nums[r]
             if(sum==0){
                 res.push([nums[i],nums[l],nums[r]])
-                while(nums[l]==nums[l+1]) l++;
-                while(nums[r]==nums[r-1]) r--;
                 l++;
                 r--;
+                while(l<r && nums[l]==nums[l-1]) l++
             }
-            else if(sum<0){
-                l++
+            else if(sum>0){
+                r--
             }
             else{
-                r--;
+                l++;
             }
         }
+        
     }
+
     return res;
 };
